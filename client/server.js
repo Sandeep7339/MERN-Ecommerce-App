00b97453780357252c03090bbbc63c85eee5cd3e
Sandeep7339 <sandeepsoni7339@gmail.com>
2024-12-31 10:28:01 +0530

    changes in server.js

--- client/server.js
+++ client/server.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 // Static folder
-app.use(express.static(path.join(__dirname, './client/build')));
+app.use(express.static(path.join(__dirname, './build')));
 
 // Routes
 app.use('/api/v1/auth', authRoutes);
@@ -35,7 +35,7 @@ app.use('/api/v1/category', categoryRoutes);
 app.use('/api/v1/product', productRoutes);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './client/build/index.html'));
+  res.sendFile(path.join(__dirname, './build/index.html'));
 });
 
 app.get("/", (req, res) => {
