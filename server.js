7b4bd94bbc70a2ba77e12ee4a981e6775c2fdf01
Sandeep7339 <sandeepsoni7339@gmail.com>
2025-07-12 22:06:19 +0530

    Changes in server.js

--- server.js
+++ server.js
@@ -24,15 +24,15 @@ app.use(morgan("dev"));
 
 
 // //static folder
-// app.use(express.static(path.join(__dirname,'./client/build')))
+app.use(express.static(path.join(__dirname,'./client/build')))
 //routes
 app.use('/api/v1/auth',authRoutes)
 app.use('/api/v1/category',categoryRoutes)
 app.use('/api/v1/product',productRoutes)
 
-// app.get('*',(req,res)=>{
-//   res.sendFile(path.join(__dirname,'./client/build/index.html'))
-// })
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname,'./client/build/index.html'))
+})
 app.get("/", (req, res) => {
   res.send({ message: "welcome to express app" });
 });
